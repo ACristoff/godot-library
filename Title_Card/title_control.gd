7accd1c883310a4eb0c81ed1509b6e6a7f37ced1
Alex Cristoffanini <acristoffanini@gmail.com>
2024-12-30 17:54:14 -0800

    title work

--- Title_Card/title_control.gd
+++ Title_Card/title_control.gd
@@ -3,6 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#get_tree().root.ContentScaleMode = 3
+	#get_viewport().Scale
 	pass # Replace with function body.
 
 
