06cff3f79d458d2bcd9bf4aa2115614d61646e05
Alex Cristoffanini <acristoffanini@gmail.com>
2025-02-07 17:41:13 -0800

    menu planning

--- Game/UI/Main_Menu/main_menu.gd
+++ Game/UI/Main_Menu/main_menu.gd
@@ -1,11 +1,17 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_start_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
